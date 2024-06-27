e36ebb916bf71364e94090c73921a5caa68635e3
aseplamri <aseplamri12@gmail.com>
2024-06-27 14:50:27 +0700

    tambah

---
+++ Test Suites/Test Suite Wikipedia.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Wikipedia</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c5256b0b-aa09-4484-8acf-90de37f4158b</testSuiteGuid>
+   <testCaseLink>
+      <guid>ba99178c-689b-41aa-bb8b-2879f9a3b534</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test/TC_Wikipedia_Search</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
